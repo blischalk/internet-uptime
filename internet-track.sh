4903b693974be71e76ae02b1138d538b0cfa94db
Brett Lischalk <blischalk@opinionlab.com>
2016-01-05 13:28:00 -0600

    Add better output with timestamps

--- internet-track.sh
+++ internet-track.sh
@@ -5,16 +5,25 @@
 
 NOW=$(date +"%m_%d_%Y")
 INTERNET_LOG=$NOW"_internet.log"
-echo 'starting internet monitoring pinging multiple hosts every 5 seconds' 2>&1 | tee -a $INTERNET_LOG
+echo -e 'starting internet monitoring pinging multiple hosts every 5 seconds' 2>&1 | tee -a $INTERNET_LOG
 
 function pingHost {
-    echo "Pinging Host: "$1 2>&1 | tee -a $INTERNET_LOG
+    echo -e "Pinging Host: $1" 2>&1 | tee -a $INTERNET_LOG
     ping -o $1 2>&1 | tee -a $INTERNET_LOG
 }
 
 while true; do
+  echo -e "----------------------------------------+" 2>&1 | tee -a $INTERNET_LOG
+  echo Starting ping sweep at: `date` 2>&1 | tee -a $INTERNET_LOG
+  echo -e '\n' 2>&1 | tee -a $INTERNET_LOG
+
   for host in google.com yahoo.com brettlischalk.com; do
-    pingHost ${host} &
+    pingHost ${host}
   done
+
+  echo -e '\n' 2>&1 | tee -a $INTERNET_LOG
+  echo Finished ping sweep at: `date` 2>&1 | tee -a $INTERNET_LOG
+  echo "----------------------------------------+" 2>&1 | tee -a $INTERNET_LOG
+
   sleep 5
 done
